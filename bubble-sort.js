a7f8ba05a7d169c968eeee76af235777c5554092
Joe Wilson <jtwilson.home@gmail.com>
2023-11-02 10:25:48 -0600

    Update bubble-sort.js

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,7 +1,7 @@
 let array = [1, 55, 33, 4, 9, 25, ]
 
 function bubbleSort(array) {
-    let y = array.length;
+    let n = array.length;
   
     for (let i = 0; i < n - 1; i++) {
       for (let x = 0; x < n - 1 - i; x++) {
